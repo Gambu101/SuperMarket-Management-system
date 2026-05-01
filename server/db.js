47f0ce877d8aac9dce92340781db7bb72f6302bd
Gambu101 <gabriel.ohalete.244627@unn.edu.ng>
2026-05-01 03:02:36 +0100

    updated email features

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 // db.js
+require("dotenv").config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root', // Use 'root' for testing, 'app_user' for production
-  password: '', // Your password
-  database: 'superinv',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'superinv',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0  
