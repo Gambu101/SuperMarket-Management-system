6837a1bb24fd8135531f6e85447b638465a91572
Gambu101 <gabriel.ohalete.244627@unn.edu.ng>
2026-05-01 20:11:03 +0100

    fix: remove broken FaArrowLeft icon reference in BackButton

--- client/superinv/src/components/BackButton.jsx
+++ client/superinv/src/components/BackButton.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import './BackButton.css'
-// import {FaArrowLeft} from 'react-icons/fa'
 import { useNavigate } from 'react-router-dom'
 
 export default function BackButton(){
     const navigate = useNavigate()
     return(
         <div>
-            <button onClick={() => navigate(-1)}><FaArrowLeft/>Go Back</button>
+            <button onClick={() => navigate(-1)}>← Go Back</button>
         </div>
     )
 }
